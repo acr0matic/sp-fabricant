01754ed0284bfdb8a175980089b270e016137ced
acr0matic <work.acr0matic@gmail.com>
2023-03-27 14:22:34 +0300

    feat: правки мобильной версии

---
+++ src/scripts/slider.js
@@ -0,0 +1,9 @@
+if (isTablet) {
+  const gallerySlider = new Swiper('.slider-gallery', {
+
+    pagination: {
+      el: '.slider-gallery .swiper-pagination',
+      clickable: true,
+    },
+  });
+}
